7b34a4d834ceeedd34232ec600c92f91362a0c50
conversayShawn <conversayshawn@gmail.com>
2024-08-17 13:19:07 -0400

    add scroll behavior for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Header.css";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <h1>Shawn Harris</h1>
+    <header className={`header ${scrolled ? "header-scrolled" : ""}`}>
+      <h1 className={`logo ${scrolled ? "logo-scrolled" : ""}`}>Shawn Harris</h1>
       <nav>
-        <ul className="nav">
+        <ul className={`nav ${scrolled ? "nav-scrolled" : ""}`}>
           <li>
             <a href="#about">ABOUT</a>
           </li>
